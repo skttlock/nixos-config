28a9a771d9a5e13a1c4be0fcaf33dd15fdc6a0cd
Joshua Foreman <joshua-codes@protonmail.com>
2026-01-06 08:31:11 -0800

    update nvim config

--- programs/nvim.nix
+++ programs/nvim.nix
@@ -3,8 +3,30 @@
 _: {
 	programs.neovim = {
 		enable = true;
+		defaultEditor = true;
 		vimAlias = true;
 		viAlias = true;
-		# configure = {};
+		configure = {
+			customRC = ''
+				set foldmethod=indent
+                set foldlevelstart=99
+                
+				set number
+
+                set autoindent
+				set expandtab
+				set shiftwidth=4
+				set tabstop=4
+
+				set ignorecase
+				set smartcase
+				set incsearch
+				set hlsearch
+
+                set hidden
+				
+                set mouse=n
+			'';
+		};
 	};
 }
