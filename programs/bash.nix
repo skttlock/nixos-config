9e33058e1e347965313944a20e1b6d415909579d
Joshua Foreman <joshua-codes@protonmail.com>
2026-01-10 20:18:33 -0800

    add lsd alias

--- programs/bash.nix
+++ programs/bash.nix
@@ -1,8 +1,11 @@
+# bash.nix
+# imported by ../programs/default.nix
 _: {
     programs.bash = {
         enable = true;
         shellAliases = {
             nrs = "nixos-rebuild switch -I nixos-config=/run/media/joshuaf/Library/System/nixos-config/configuration.nix --sudo";
+            lst = "lsd --tree";
         };
     };
 }
