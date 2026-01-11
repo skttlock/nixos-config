d8b4c4d87266f569d6708680dd33a0be8bf1b28c
Joshua Foreman <joshua-codes@protonmail.com>
2026-01-10 20:20:07 -0800

    indenting

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,26 @@
 { config, pkgs, ... }: {
-	imports = [
-		./hardware-configuration.nix
-		./services/default.nix
-		./programs/default.nix
-		./users/joshuaf.nix
-	];
+    imports = [
+        ./hardware-configuration.nix
+            ./services/default.nix
+            ./programs/default.nix
+            ./users/joshuaf.nix
+            # ./storage.nix
+    ];
 
-	nix.settings.experimental-features = ["nix-command"];
-	nixpkgs.config.allowUnfree = true;	# allow unfree packages
+    nix.settings.experimental-features = ["nix-command"];
+    nixpkgs.config.allowUnfree = true;	# allow unfree packages
 
-	# bootloader
-	boot.loader = {
-		systemd-boot.enable = true;
-		efi.canTouchEfiVariables = true;
-	};
+# bootloader
+        boot.loader = {
+            systemd-boot.enable = true;
+            efi.canTouchEfiVariables = true;
+        };
 
-	# networking
-	networking = {
-    networkmanager.enable = true;
+# networking
+    networking = {
+        networkmanager.enable = true;
         hostName = "nixos"; # Define your hostname.
-        nameservers = [ "192.168.7.248" ];
+            nameservers = [ "192.168.7.248" ];
         # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
         # Configure network proxy if necessary
         # proxy.default = "http://user:password@proxy:port/";
@@ -31,30 +32,30 @@
         # firewall.enable = false;
     };
 
-	# locale & timezone
-	# Set your time zone.
-	time.timeZone = "America/Los_Angeles";
-	# Select internationalisation properties.
-	i18n = {
-		defaultLocale = "en_US.UTF-8";
-		extraLocaleSettings = {
-			LC_ADDRESS = "en_US.UTF-8";
-			LC_IDENTIFICATION = "en_US.UTF-8";
-			LC_MEASUREMENT = "en_US.UTF-8";
-			LC_MONETARY = "en_US.UTF-8";
-			LC_NAME = "en_US.UTF-8";
-			LC_NUMERIC = "en_US.UTF-8";
-			LC_PAPER = "en_US.UTF-8";
-			LC_TELEPHONE = "en_US.UTF-8";
-			LC_TIME = "en_US.UTF-8";
-		};
-	};
+# locale & timezone
+# Set your time zone.
+    time.timeZone = "America/Los_Angeles";
+# Select internationalisation properties.
+    i18n = {
+        defaultLocale = "en_US.UTF-8";
+        extraLocaleSettings = {
+            LC_ADDRESS = "en_US.UTF-8";
+            LC_IDENTIFICATION = "en_US.UTF-8";
+            LC_MEASUREMENT = "en_US.UTF-8";
+            LC_MONETARY = "en_US.UTF-8";
+            LC_NAME = "en_US.UTF-8";
+            LC_NUMERIC = "en_US.UTF-8";
+            LC_PAPER = "en_US.UTF-8";
+            LC_TELEPHONE = "en_US.UTF-8";
+            LC_TIME = "en_US.UTF-8";
+        };
+    };
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "25.11"; # Did you read the comment?
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.11"; # Did you read the comment?
 }
